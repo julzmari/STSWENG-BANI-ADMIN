103b4fc22fbe61758ab362ea712fa38078c49708
John Uriel Dela Cruz <136968627+urieldc19@users.noreply.github.com>
2024-10-04 18:50:37 +0800

    Changed backend

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,8 +4,9 @@ const dotenv = require ('dotenv')
 
 dotenv.config()
 
-const connectDB = async () => {
 
+const connectDB = async () => {
+   
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Connected to MongoDB ${conn.connection.host} `)
