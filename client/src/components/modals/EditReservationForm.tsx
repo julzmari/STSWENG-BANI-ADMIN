fcf8ca3ae290a456a35f807aa97e6e6bd567525f
John Uriel Dela Cruz <136968627+urieldc19@users.noreply.github.com>
2024-10-13 20:36:17 +0800

    Added client notes column and editing admin notes in modal

--- client/src/components/modals/EditReservationForm.tsx
+++ client/src/components/modals/EditReservationForm.tsx
@@ -1,32 +1,33 @@
-import { Button, Checkbox, Group, NativeSelect, NumberInput } from '@mantine/core';
+import { Button, Group, NativeSelect, NumberInput, Textarea } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { reservationResponseData } from '../tables/TableCreator';
 import { useDisclosure } from '@mantine/hooks';
 
 export function EditReservationEntry({reservationEntry} : {reservationEntry: reservationResponseData}) {
 
-    const [loading, { open, close }] = useDisclosure();
+    //const [loading, { open, close }] = useDisclosure();
 
     const form = useForm({
         mode: 'uncontrolled',
         initialValues: {
         totalAmount: reservationEntry.totalAmount,
         amountPaid: reservationEntry.amountPaid,
-        arrivalStatus: reservationEntry.arrivalStatus
+        arrivalStatus: reservationEntry.arrivalStatus,
+        adminNotes: reservationEntry.adminNotes,
         },
     });
 
 
     const handleSubmit = () => {
-        open()
+        //open()
 
         try {
-            form.getValues()
+            console.log(form.getValues())
         } catch (error) {
             console.error(error)
         }
 
-        close()
+       // close()
     }
 
 
@@ -56,6 +57,13 @@ export function EditReservationEntry({reservationEntry} : {reservationEntry: res
                 //onChange={(event) => setValue(event.currentTarget.value)}
                 data={['Booked', 'Checked-in', 'Checked-out']}
             />
+
+            <Textarea
+                label="Admin Notes"
+                key={form.key('adminNotes')}
+                {...form.getInputProps('adminNotes')}
+            />
+
 
             <Group justify="flex-end" mt="md">
                 <Button type="submit">Submit</Button>
