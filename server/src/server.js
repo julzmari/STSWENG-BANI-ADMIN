791ee5ab161804295ab2152ab45fdc7b5113f2f3
John Uriel Dela Cruz <136968627+urieldc19@users.noreply.github.com>
2024-10-14 10:42:57 +0800

    Removed sample data

--- server/src/server.js
+++ server/src/server.js
@@ -9,60 +9,6 @@ app.use(express.static("public"));
 app.use(express.json())
 app.use(bodyParser.json());
 
-const roomData = require("./models/sampledata/roomData.json")
-const clientData = require("./models/sampledata/clientData.json")
-const reservationData = require("./models/sampledata/reservationData.json")
-
-//check if the data has been loaded
-app.get('/', async function (req, res) {
-
-  const clientCount = await Client.countDocuments();
-        if (clientCount === 0) {
-
-            Client.insertMany(clientData)
-            .then((clients) => {
-                console.log('Imported clients:', clients);
-            })
-            .catch((err) => {
-                console.error('Error importing data:', err);
-            });
-
-        } else {
-            console.log('Sample client data already imported.');
-        }
-    
-  const roomCount = await Room.countDocuments();
-        if (roomCount === 0) {
-            
-            Room.insertMany(roomData)
-            .then((rooms) => {
-                console.log('Imported rooms:', rooms);
-            })
-            .catch((err) => {
-                console.error('Error importing data:', err);
-            });
-            
-        } else {
-            console.log('Sample room data already imported.');
-        }
-
-        // Check and create Reservation if none exists
-        const reservationCount = await Reservation.countDocuments();
-        if (reservationCount === 0) {
-            
-            Reservation.insertMany(reservationData)
-            .then((reservations) => {
-                console.log('Imported reservations:', reservations);
-            })
-            .catch((err) => {
-                console.error('Error importing data:', err);
-            });
-
-        } else {
-            console.log('Sample reservation data already imported.');
-        }
-})
-
 //gets reservations from the database and brings them to the reservationData page
 app.get ('/api/get-reservations', async function (req, res){
 
