31e8a5357ca2f75992957f94ee108a9295930b94
Leomarc <leomarc_alonzo@dlsu.edu.ph>
2024-10-17 14:54:56 +0800

    modified vite config for port and api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 5173,  
+    host: '0.0.0.0',                 
     proxy: {
-      '/api':'http://localhost:3000'
+      '/api':'https://bani-admin-api'
     }
   },
   plugins: [react()]
