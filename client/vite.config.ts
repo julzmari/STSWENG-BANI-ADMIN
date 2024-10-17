175653487d8d174686072904e14ad9fed2f99257
Leomarc <leomarc_alonzo@dlsu.edu.ph>
2024-10-17 15:25:08 +0800

    added start on server, configured client network

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5173,  
+    port: process.env.PORT || 5173, // render process
     host: '0.0.0.0',                 
     proxy: {
-      '/api':'https://bani-admin-api'
+      '/api':'https://bani-admin-api.onrender.com'
     }
   },
   plugins: [react()]
